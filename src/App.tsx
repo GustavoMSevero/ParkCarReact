5726b733e19391a09b444efd287885b81aaafbd4
Velcis Ribeiro <velcisribeirodj@gmail.com>
2022-11-21 17:06:59 -0300

    updated route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import './App.css'
+import "./App.css";
 
-import Routes from './Routes/routes';
+import Routes from "./routes";
 
-import {
-  RouterProvider,
-} from 'react-router-dom';
+import { RouterProvider } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
       <RouterProvider router={Routes} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
