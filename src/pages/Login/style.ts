6424ecde7236b22bdd5c3c28b8fec2221bc5b7d2
Velcis Ribeiro <velcisribeirodj@gmail.com>
2022-11-21 17:07:25 -0300

    styled login

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,6 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Container = styled.view`
-    flexDirection: 'row';
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    .leftSide {
+        flex: 1;
+        
+        img {
+            object-fit: cover;
+            width: 100%;
+            height: 100vh;
+        }
+    }
+
+    .rightSide {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+    }
 `;
