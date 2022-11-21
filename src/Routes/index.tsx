3afa044da8601e225cb172324883bb93a0f57e2a
Velcis Ribeiro <velcisribeirodj@gmail.com>
2022-11-21 17:07:33 -0300

    new routes js

---
+++ src/Routes/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import Login from "../pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+export default router;
