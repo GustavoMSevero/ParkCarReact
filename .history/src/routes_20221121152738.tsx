50187c3a4ab5b42b900f241bfc89718c3b663f05
Gustavo M Severo <gustavo@Gustavos-MacBook-Pro.local>
2022-11-22 15:15:34 -0300

    Update package.json and yarn.lock.

--- .history/src/routes_20221121152738.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import {
-    createBrowserRouter
-} from 'react-router-dom';
-
-const router = createBrowserRouter([
-    
-])
