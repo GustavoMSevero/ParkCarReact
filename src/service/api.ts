9c92d450621492c395843f4b5f88169b14da698f
Gustavo M Severo <gustavo@Gustavos-MacBook-Pro.local>
2022-11-24 16:25:41 -0300

    Adjusted base url.

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,8 +2,8 @@ import React from "react";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'localhost:3000/api',
-    timeout: 4000,
+    baseURL: 'http://localhost:3333/',
+    // timeout: 4000,
 });
 
 export default api;
