ee38310c24cc30072e25b52bac6636aeb293e135
Gustavo M Severo <gustavo@Gustavos-MacBook-Pro.local>
2023-01-19 14:25:28 -0300

    Created page for update password.

---
+++ src/pages/UpdatePassword/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const UpdatePassword: React.FC = () => {
+  return <h1>Update Password Screen</h1>;
+};
+
+export default UpdatePassword;
