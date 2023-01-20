0b86997361924528efe18ccb6cdbb6ff674ca689
Velcis Ribeiro <velcisribeirodj@gmail.com>
2023-01-20 17:03:56 -0300

    TextInput for ReactHOOKFORM

---
+++ src/components/Inputs/TextInput.tsx
@@ -0,0 +1,32 @@
+import { TextField } from "@mui/material";
+import {
+  Controller,
+  ControllerFieldState,
+  ControllerRenderProps,
+  useFormContext,
+} from "react-hook-form";
+
+export const TextInputRHF = ({ name, inputProps, ...rest }: any) => {
+  const { control } = useFormContext();
+
+  const renderInputForm = ({
+    field: { onChange, onBlur, value, ref },
+    fieldState: { error },
+  }: {
+    field: ControllerRenderProps;
+    fieldState: ControllerFieldState;
+  }) => {
+    console.log(value);
+    return (
+      <TextField
+        inputProps={inputProps}
+        onChange={onChange}
+        value={value}
+        {...rest}
+        fullWidth
+      />
+    );
+  };
+
+  return <Controller name={name} control={control} render={renderInputForm} />;
+};
