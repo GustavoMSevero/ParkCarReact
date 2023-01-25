e24d286996defb2a2fa46e22e0904a3928d54615
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-01-25 17:42:43 -0300

    Register page already working.

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -4,7 +4,10 @@ import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { TextInputRHF } from "../../components/Inputs/TextInput";
+import api from "../../services/api";
 import { Container } from "./style";
+import { useAtom } from "jotai";
+import { ownerAtom } from "../../store/ownerStore";
 
 // import { Container } from './styles';
 
@@ -16,7 +19,28 @@ const Register: React.FC = () => {
     ownerPassword: "",
   };
 
+  const [owner, setOwner] = useAtom(ownerAtom);
+
   const methods = useForm();
+
+  const onError = (error: any) => {
+    console.log("ERROR:::", error);
+  };
+
+  const onSubmit = (values: any) => {
+    console.log(values);
+    const ownerParking = api.post("ownerParking", values).then((response) => {
+      console.log(response)
+      setOwner({
+        ownerEmail: response.data.owner_email,
+        ownerName: response.data.owner_name,
+      });
+      navigate("/");
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  };
 
   return (
     <Container>
@@ -28,9 +52,9 @@ const Register: React.FC = () => {
           <FormProvider {...methods}>
             <TextInputRHF name="name" label="Nome do Proprietário" />
             <TextInputRHF name="email" label="E-mail" />
-            <TextInputRHF name="password" label="Senha" />
+            <TextInputRHF name="password" type="password" label="Senha" />
 
-            <Button variant="contained" color="success">
+            <Button variant="contained" color="success" onClick={methods.handleSubmit(onSubmit, onError)}>
               Cadastrar
             </Button>
 
