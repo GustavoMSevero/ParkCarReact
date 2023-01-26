bd88d4b0d526f5ff5b7bef35adb97bc7e81691db
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-01-26 18:33:29 -0300

    Added id owner parking.

--- src/store/ownerStore.ts
+++ src/store/ownerStore.ts
@@ -2,4 +2,4 @@ import { atomWithStorage, createJSONStorage } from "jotai/utils";
 
 const storage = createJSONStorage(() => localStorage)
 
-export const ownerAtom = atomWithStorage('owner', { ownerName: '', ownerEmail: ''}, storage);
+export const ownerAtom = atomWithStorage('owner', { id_owner_parking: '', ownerName: '', ownerEmail: ''}, storage);
