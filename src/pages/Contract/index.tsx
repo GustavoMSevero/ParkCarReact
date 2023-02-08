38322b6605a5d4bdaa2d1bb7b1c786e0b44fede2
Gustavo M SEVERO <gustavo.msevero@gmail.com>
2023-02-08 14:06:41 -0300

    Imported ownerAtom to register in contracts.

--- src/pages/Contract/index.tsx
+++ src/pages/Contract/index.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import MainCard from "../../components/MainCard";
 import { TextInputRHF } from "../../components/Inputs/TextInput";
 import { Button, Divider, Grid, Table } from "@mui/material";
 import { TableContract } from "./components/Table";
+import api from "../../services/api";
+import { useAtom } from "jotai";
+import { ownerAtom } from "../../store/ownerStore";
 
 // import { Container } from './styles';
 
 const Contract: React.FC = () => {
+  const [owner, setOwner] = useAtom(ownerAtom);
+
+  useEffect(() => {
+    console.log(owner)
+  },[]);
+
   const contracts = [
     {
       id: 1,
@@ -20,8 +29,12 @@ const Contract: React.FC = () => {
   };
 
   const onSubmit = (values: any) => {
-    values.option = "register contract";
-    console.log(values);
+    values.idOwnerParking = owner.id_owner_parking;
+    const registerContract = api.post("customerContracts", values).then((response) => {
+      console.log(response)
+    }).catch((error) => {
+      console.log(error)
+    });
   };
 
   const methods = useForm();
@@ -31,11 +44,11 @@ const Contract: React.FC = () => {
       <FormProvider {...methods}>
         <Grid container spacing={2}>
           <Grid item xs={4}>
-            <TextInputRHF name="contract" label="Contrato" />
+            <TextInputRHF type="text" name="contractType" label="Contrato" />
           </Grid>
 
           <Grid item xs={12}>
-            <Button variant="contained" color="success">
+            <Button variant="contained" type="submit" color="success" onClick={methods.handleSubmit(onSubmit, onError)}>
               Cadastrar
             </Button>
           </Grid>
